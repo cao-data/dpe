4bbecd47b0d9c85026e8d9645850d6959b07d592
Guillermo Croppi <guillermocroppi@gmail.com>
2022-08-25 13:52:29 -0300

    Update Gtag and added event trackers

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,10 +81,11 @@ export default {
       anonymize_ip: true, // anonymize IP
       send_page_view: false, // might be necessary to avoid duplicated page track on page reload
       linker: {
-        domains: ['guillecro.github.io/data-graphs-diego']
+        domains: ['guillecro.github.io/data-graphs-dpe']
       }
     },
-    debug: true, // enable to track in dev mode
+    // debug: process.env.NODE_ENV !== 'production', // enable to track in dev mode
+    debug: false, // enable to track in dev mode
     disableAutoPageTrack: false // disable if you don't want to track each page route with router.afterEach(...).
   }
 }
