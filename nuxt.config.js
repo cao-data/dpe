1f847cecb98be639e99c5a5f33f1f40c02bb5afd
Guillermo Croppi <guillermocroppi@gmail.com>
2023-02-02 16:17:42 -0300

    Updated project path

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/dpe/'
+    base: '/spe/'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
